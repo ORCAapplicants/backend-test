d6964fbe84a4dede66902292b5b34cf83cedf4a7
David Rojo <dsrojo10@gmail.com>
2022-06-05 00:36:19 -0500

    Primera version estable de la aplicacion

--- code/main.py
+++ code/main.py
@@ -5,6 +5,7 @@ David Santiago Rojo C. (dsrojo10@gmail.com)
 '''
 
 # librerias implementadas
+# import os
 import datetime
 import sqlite3
 import datetime
@@ -18,9 +19,19 @@ def calcularTiempo(placa):
     t = (t2 - t1)
     return t
 
+def calcularMinutos(placa):
+    t1 = cars_init[placa]
+    t2 = cars_close[placa]
+    t = (t2 - t1)
+    minutes = t.total_seconds() / 60
+    minutes = round(minutes,2)
+    return minutes
+
 """" ------------ BASE DE DATOS ------------------- """
 # Se crea la tabla
 autosDB.crear_tabla_carros()
+conn = sqlite3.connect('parqueadero.db')
+c = conn.cursor()
 print("La conexion con la base de datos fue exitosa!\n")
 
 # # Creamos 4 objetos de tipo Auto(placa, residente, oficial, tiempo).
@@ -50,6 +61,18 @@ cars_close = {} # Diccionario para la hora de salida del parqueadero
 carsOF = [] # Vector para listar los autos oficiales
 carsRSD = [] # Vector para listar los autos residentes
 
+"""" CARGAR DATOS DE LA BD A LOS VECTORES """
+# Se cambia el estatus de oficial a no oficial
+aux = autosDB.seleccionar_por_estatus("oficial")
+for i in range(0, len(aux)):
+    carsOF.append(aux[i][0])
+
+# Se reinicia el tiempo de parqueo de residentes
+aux = autosDB.seleccionar_por_estatus("residente")
+for i in range(0, len(aux)):
+    carsRSD.append(aux[i][0])
+""" ------------------------------------- """
+
 while True:
     #MENU
     print("\n------------PARQUEADERO------------\n")
@@ -65,9 +88,6 @@ while True:
     print("Seleccione una opcion:")
     aux = input()
 
-    if(aux == '7' or aux == 7):
-        break
-    
     # REGISTRAR ENTRADA
     if(aux == '1' or aux == 1):
         print("Ingrese la placa del auto ingresado:") # Se solicita la placa
@@ -87,41 +107,36 @@ while True:
         placa=placa.upper() # Se convierte a MAYUS para mantener coherencia entre datos
         cars_close[placa] = datetime.datetime.now() # Se almacena los datos de salida del parqueadero
         print(cars_close) # Se muestra el diccionario con horas de salida
-        # registrarTiempo(placa, cars_close)
-        
-        # if(placa in carsOF):
-            # cars_time[placa] = calcularTiempo(str(placa))
-        # if(placa in carsRSD):
-        #     cars_time[placa] = calcularTiempo(str(placa))
-        # else:
-        #     deuda = calcularTiempo(placa)
-        #     print("---------------------------------------")
-        #     print("El tiempo que estuvo fue:")
-        #     print(deuda) 
-        #     print("---------------------------------------")
+
         if(placa in carsOF):
             print("OFICIAAAAAAAAAAL.")
             cars_time[placa] = calcularTiempo(str(placa)) # se almacena el tiempo que estuvo
+            minutes = calcularMinutos(placa)
+            autosDB.actualizar_tiempo(placa, minutes)
         if(placa in carsRSD):
-            cars_time[placa] = calcularTiempo(str(placa)) # se almacena el tiempo que estuvo
+            cars_time[placa] = calcularMinutos(str(placa)) # se almacena el tiempo que estuvo
+            minutes = calcularMinutos(placa)
+            autosDB.actualizar_tiempo(placa, minutes)
             print("RESIDENTESSSSSS.")
         if(placa not in carsOF and placa not in carsRSD):
-            deuda = calcularTiempo(placa)
+            minutos = calcularMinutos(placa)
+            # minutos = deuda.total_seconds / 60
+            # minutos = round(minutos, 2)
             print("---------------------------------------")
             print("El tiempo que estuvo fue:")
-            print(deuda) 
+            print(minutos) 
+            print("El pago es:")
+            print(minutos*0.5," MXN") 
             print("---------------------------------------")
             print("XXXXXXXXXXXXXXXXX")
-
-
-
         
     # DAR DE ALTA VEHICULO OFICIAL
     if(aux == '3' or aux == 3):
         print("Ingrese la placa del auto a registrar:") # Se solicita la placa
         placa=input() # Se recibe el dato por consola
         placa=placa.upper() # Se convierte a MAYUS para mantener coherencia entre datos
-        carsOF.append(placa)
+        if(placa not in carsOF):
+            carsOF.append(placa)    
         autosDB.actualizar_oficial(placa, '1') # se actualiza el estatus en la base de datos a carro oficial
         print(carsOF)
     
@@ -130,7 +145,8 @@ while True:
         print("Ingrese la placa del auto a registrar:") # Se solicita la placa
         placa=input() # Se recibe el dato por consola
         placa=placa.upper() # Se convierte a MAYUS para mantener coherencia entre datos
-        carsRSD.append(placa)
+        if(placa not in carsRSD):
+            carsRSD.append(placa)
         autosDB.actualizar_residente(placa, '1') # se actualiza el estatus en la base de datos a carro residente
         print(carsRSD)
     
@@ -140,14 +156,49 @@ while True:
         cars_time.clear()
         cars_init.clear()
         cars_close.clear()
-        autosDB.eliminar_tabla()
+
+        # Se cambia el estatus de oficial a no oficial
+        aux = autosDB.seleccionar_por_estatus("oficial")
+        for i in range(0, len(aux)):
+            autosDB.actualizar_oficial(aux[i][0], '0')
+        
+        # Se reinicia el tiempo de parqueo de residentes
+        aux = autosDB.seleccionar_por_estatus("residente")
+        for i in range(0, len(aux)):
+            autosDB.actualizar_tiempo(aux[i][0], 0)
 
     # PAGOS RESIDENTES
     if(aux == '6' or aux == 6):
-        print("PLACA:          TIEMPO(min):        DEUDA")
-        for i in carsRSD:
-            print(i, "       ",cars_time[i], "    $$$")
+        print("Ingrese el nombre para el archivo de cuenta residentes:")
+        nomArchivo = input()
+        nomArchivo = nomArchivo+".txt"
+
+        # Seleccionamos todos los residentes
+        c.execute("SELECT * FROM tiempos_mes WHERE residente=?", (1,))
+        carros = c.fetchall()
+        print(carros)
+
+        file = open(nomArchivo, "w")
+        # file.write("PLACA:          TIEMPO(min):        DEUDA" + os.linesep)
+        file.write("PLACA:      TIEMPO(min):      DEUDA:\n")
+        # print("PLACA:          TIEMPO(min):        DEUDA")
+        # for i in carsRSD:
+        #     if(i in cars_time):
+        #         contenido = i+ "       " + str(cars_time[i]) + "        " + str(cars_time[i]*0.05) + " MXN."
+        #         print(i, "       ",cars_time[i], "        ", str(cars_time[i]*0.05), " MXN.")
+        #         file.write(contenido + os.linesep)
+        # file.close()
+
+        for i in carros:
+                contenido = i[0]+ "            " + str(i[3]) + "        " + str(i[3]*0.05) + " MXN.\n"
+                # print(i, "       ",cars_time[i], "        ", str(cars_time[i]*0.05), " MXN.")
+                # file.write(contenido + os.linesep)
+                file.write(contenido)
+        file.close()
         
+    if(aux == '7' or aux == 7):
+        break
+
     # LISTAR TIEMPOS
     if(aux == '8' or aux == 8):
         print("Times:")
@@ -156,15 +207,14 @@ while True:
         print(cars_init)
         print("Times close:")
         print(cars_close)
-        autosDB.seleccionar_todos()
+        print(autosDB.seleccionar_todos())
 
-    # CALCULAR TIEMPO DENTR DEL PARQUEADERO
+    # CALCULAR TIEMPO DENTRO DEL PARQUEADERO
     if(aux == '9' or aux == 9):
         print("Ingrese la placa del auto")
         placa = input()
         placa = placa.upper() # Se convierte a MAYUS para mantener coherencia entre datos
         print(calcularTiempo(placa))
-
 
 
 
